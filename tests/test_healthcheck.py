731531af8ed681ac8cac3c61e59a0a2c28a88400
vitalykamynin <vitaly.kamynin@gmail.com>
2024-04-01 22:58:57 -0400

    tcid1

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -10,7 +10,6 @@ def test_healthcheck():
 
 @pytest.mark.tcid1
 def test_check_ENV():
-    env_db = os.getenv('DB_NAME')
+    env_db = os.environ['DB_NAME']
     log.info(env_db)
-    assert env_db == "ASK_DATABASE"
 
