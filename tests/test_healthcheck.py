129a7c74d56abed850e48b64d5397d2f6f5dbc61
vitalykamynin <vitaly.kamynin@gmail.com>
2024-04-01 23:09:43 -0400

    tcid1

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -11,5 +11,6 @@ def test_healthcheck():
 @pytest.mark.tcid1
 def test_check_ENV():
     # env_db = os.getenv('GITHUB_REPOSITORY_OWNER')
-    log.info(os.getenv('GITHUB_REPOSITORY_OWNER'))
-
+    log.info(os.getenv('DB_NAME'))
+    log.info(os.getenv('GITHUB_JOB'))
+    log.info(os.getenv('PERFLOG_LOCATION_SETTING'))
